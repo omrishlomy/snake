bf33dc1422a6a562bb1180d6e65e953fb24baee3
omrishlomy <46932190+omrishlomy@users.noreply.github.com>
2023-06-10 16:58:05 +0300

    Update snake_main.py

--- snake_main.py
+++ snake_main.py
@@ -11,12 +11,17 @@ def main_loop(gd: GameDisplay, args: argparse.Namespace) -> None:
     # DRAW BOARD
     game.draw_board(gd)
     # END OF ROUND 0
-    while not game.is_over():
+    while not game.is_over(gd):
         # CHECK KEY CLICKS
         key_clicked = gd.get_key_clicked()
         game.read_key(key_clicked)
+        game.change_direction()
+        if game.check_in_board(gd):
+            continue
         # UPDATE OBJECTS
         game.update_objects()
+        if game.eat_snake():
+            return
         # DRAW BOARD
         game.draw_board(gd)
         # WAIT FOR NEXT ROUND:
