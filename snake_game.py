e87963d3089f50b58a92cc43dde0758fe777acd2
galshtu <136000490+galshtu@users.noreply.github.com>
2023-06-14 13:07:50 +0300

    Update snake_game.py

--- snake_game.py
+++ snake_game.py
@@ -10,22 +10,77 @@ import math
 class SnakeGame:
 
     def __init__(self, args: argparse.Namespace) -> None:
-        self.__x = args.height//2
-        self.__y = args.width//2
+        # game settings:
         self.__width = args.width
         self.__height = args.height
         self.__num_apples, self.__num_walls = args.apples, args.walls
         self.__key_clicked = None
-        self.__snake_coordinates = [(self.__x, self.__y - 2), (self.__x, self.__y - 1), (self.__x, self.__y)]
+        self.score = 0
+
+        # snake's head coordinates:
+        self.__x = args.width // 2
+        self.__y = args.height // 2
         self.__head_direction = 'Up'
+
+        # objects:
+        self.__snake_coordinates = [(self.__x, self.__y - 2), (self.__x, self.__y - 1), (self.__x, self.__y)]
         self.__walls = []
         self.__apples = []
         self.__lengthenning = 0
-        self.score = 0
+
+
+
+    # manage board:
+
+    def draw_board(self, gd: GameDisplay) -> None:
+        for apple in self.__apples:
+            gd.draw_cell(apple[0], apple[1], "green")
+
+        for cell in range(len(self.__snake_coordinates)):
+            # if  self.__snake_coordinates[cell][0]<
+            gd.draw_cell(self.__snake_coordinates[cell][0], self.__snake_coordinates[cell][1], "black")
+
+        for wall in self.__walls:
+            for (x, y) in wall.coordinates:
+                gd.draw_cell(x, y, "blue")
+
+
+    def update_objects(self, round)-> None:
+        self.walls_updater(round)
+        self.update_snake()
+        self.las_manzanas(self.__apples)
+        # if self.wall_smash_snake():
+        #     self.__snake_coordinates.pop(-1)
+
+
+
+    def check_in_board(self,gd:GameDisplay):
+        """check if the snake is in the limits of the board, draw the relevant cell while 
+            the snake reaches one of the board limits"""
+
+        for cell in range(len(self.__snake_coordinates)):
+            if self.__snake_coordinates[cell][0] > self.__width or (
+                    self.__snake_coordinates[cell][1] > self.__height):
+                self.__snake_coordinates = self.__snake_coordinates[:-1]
+                return True
+
+            if self.__snake_coordinates[cell][0] < 0 or self.__snake_coordinates[cell][1] < 0:
+                self.__snake_coordinates = self.__snake_coordinates[:-1]
+                return True
+
+
+    def is_over(self,gd:GameDisplay,rounds, args: argparse.Namespace) -> bool:
+        if self.check_in_board(gd) or rounds == args.rounds or self.eat_snake() or self.wall_smash_snake():
+            # show_score #function that prints the score prerrylli
+            return True
+
+    def end_round(self) -> None:
+        pass
 
 
 
 
+    # snake's navigation
     def read_key(self, key_clicked: Optional[str])-> None:
         self.__key_clicked = key_clicked
 
@@ -45,19 +100,19 @@ class SnakeGame:
             self.__head_direction = self.__head_direction
 
 
-
+    # snake's actions:
     def snake_eat_apple(self):
         wnt_to_go = (self.__x,self.__y)
         if wnt_to_go in self.__apples:
             self.__apples.remove(wnt_to_go)
             return True
 
+
     def wall_smash_snake(self):
         head = (self.__x,self.__y)
         for wall in self.__walls:
             if head in wall.coordinates:
                 return True
-
 
 
 
@@ -78,13 +133,12 @@ class SnakeGame:
             self.__x -= 1
         elif (self.__head_direction == 'Right') and (self.__x < self.__width):
             self.__x += 1
-        elif (self.__head_direction == 'Up') and (self.__x < self.__height):
+        elif (self.__head_direction == 'Up') and (self.__y < self.__height):
             self.__y += 1
-        elif (self.__head_direction == 'Down') and (self.__x > 0):
+        elif (self.__head_direction == 'Down') and (self.__y > 0):
             self.__y -= 1
         wnt_to_go = (self.__x, self.__y)
-        if self.eat_snake():
-            return False
+
         if self.__head_direction != None:
             if self.snake_eat_apple():
                 self.score += int(math.sqrt((len(self.__snake_coordinates))))
@@ -98,53 +152,7 @@ class SnakeGame:
 
 
 
-    def draw_board(self, gd: GameDisplay) -> None:
-        for apple in self.__apples:
-            gd.draw_cell(apple[0],apple[1],"green")
-
-        for cell in range(len(self.__snake_coordinates)):
-                gd.draw_cell(self.__snake_coordinates[cell][0], self.__snake_coordinates[cell][1], "black")
-
-        for wall in self.__walls:
-            for (x,y) in wall.coordinates:
-                gd.draw_cell(x, y, "blue")
-
-
-    def update_objects(self, round)-> None:
-        self.walls_updater(round)
-        self.update_snake()
-        self.las_manzanas(self.__apples)
-        # if self.wall_smash_snake():
-        #     self.__snake_coordinates.pop(-1)
-
-
-
-    def check_in_board(self,gd:GameDisplay):
-        """check if the snake is in the limits of the board, draw the relevant cell while 
-            the snake reaches one of the board limits""" #need to chane width and height to args
-        for cell in range(len(self.__snake_coordinates)):
-            if (self.__snake_coordinates[cell][0] >= self.__width) or (
-                    self.__snake_coordinates[cell][1] >= self.__height):
-                    self.__snake_coordinates = self.__snake_coordinates[:-1]
-                    self.draw_board(gd)
-                    return True
-            if self.__snake_coordinates[cell][0] <= 0 or self.__snake_coordinates[cell][1] <= 0:
-                    self.__snake_coordinates = self.__snake_coordinates[:-1]
-                    self.draw_board(gd)
-                    return True
-
-
-    def is_over(self,gd:GameDisplay,rounds, args: argparse.Namespace) -> bool:
-        if self.check_in_board(gd) or rounds == args.rounds or self.eat_snake() or self.wall_smash_snake():
-            # show_score #function that prints the score prerrylli
-            return True
-
-    def end_round(self) -> None:
-        pass
-
-#walls functions:
-
-    #removes out of board walls coordinations
+    # walls functions:
     def walls_coo_filter(self):
         wall_removed = False
         for wall in self.__walls:
@@ -161,14 +169,18 @@ class SnakeGame:
 
 
     def walls_updater(self, round):
+        # move walls
+        new_coo = []
         if round % 2 == 0:
-            new_coo = []
             for wall in self.__walls:
                 new_coo.append(wall.move())
-            hitted = self.wall_cut_things(new_coo)
-            if hitted != None:
-                self.__snake_coordinates = self.__snake_coordinates[hitted + 1:]
-        if self.walls_coo_filter():
+
+        # cut snake if necessary
+        hitted = self.wall_cut_things(new_coo)
+        if hitted != None:
+            self.__snake_coordinates = self.__snake_coordinates[hitted + 1:]
+
+        if self.walls_coo_filter(): # checks if there was a wall that left the board during the filtering process
             self.wall_adder()
         self.wall_adder()
 
@@ -181,7 +193,6 @@ class SnakeGame:
             if self.new_wall_checker(new_wall.coordinates):
                 self.__walls.append(new_wall)
                 self.walls_coo_filter()
-
 
 
 
