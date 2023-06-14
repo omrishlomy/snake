3659c278865497301a8d4a71711f349d68518bb1
galshtu <136000490+galshtu@users.noreply.github.com>
2023-06-14 13:06:16 +0300

    Update snake_main.py

--- snake_main.py
+++ snake_main.py
@@ -11,28 +11,40 @@ def main_loop(gd: GameDisplay, args: argparse.Namespace) -> None:
     round_num = 0
     # DRAW BOARD
     game.update_objects(round_num)
+    if args.debug == True:
+        game.__snake_coordinates = []
     game.draw_board(gd)
     # END OF ROUND 0
     gd.end_round()
-    round_num += 1
     while not game.is_over(gd,round_num,args):
+        round_num += 1
+
         # CHECK KEY CLICKS
         key_clicked = gd.get_key_clicked()
         game.read_key(key_clicked)
         game.change_direction()
         # UPDATE OBJECTS
         game.update_objects(round_num)
-        # if game.check_in_board(gd):
-        #     continue
+        if game.check_in_board(gd):
+            continue
         # if game.eat_snake():
         #     continue
         # DRAW BOARD
         game.draw_board(gd)
         # WAIT FOR NEXT ROUND:
-        round_num += 1
+        # round_num += 1
         game.end_round()
         gd.show_score(game.score)
         gd.end_round()
+    # one_round(gd,args,round_num)
+
+def one_round(gd: GameDisplay, args: argparse.Namespace,rounds):
+    game.update_objects(rounds)
+    game.draw_board(gd)
+    rounds+=1
+    game.end_round()
+    gd.show_score(game.score)
+    gd.end_round()
 
 if __name__ == "__main__":
     print("You should run:\n"
